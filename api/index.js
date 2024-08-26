1c9268718d47da2a7636bef743d8c7ad932c19a7
Oscar Wanga <87162511+oscarwanga98@users.noreply.github.com>
2024-08-26 16:24:13 +0300

    Update index.js

--- api/index.js
+++ api/index.js
@@ -16,8 +16,8 @@ const secret = process.env.SECRET_KEY;
 
 const salt = bcrypt.genSaltSync(10);
 
-
-app.use(cors({ credentials: true, origin: process.env.CLIENT_ORIGIN }));
+app.use(cors());
+// app.use(cors({ credentials: true, origin: process.env.CLIENT_ORIGIN }));
 app.use(express.json());
 app.use(cookieParser());
 app.use("/uploads", express.static(__dirname + "/uploads"));
