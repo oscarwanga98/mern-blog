bd165e7740721e778c0df73b0b7ff806b53a11fb
Oscar Wanga <87162511+oscarwanga98@users.noreply.github.com>
2024-08-27 11:29:36 +0300

    Update Post.js

--- client/src/Post.js
+++ client/src/Post.js
@@ -9,7 +9,7 @@ export default function Post({_id,title,summary,cover,content,createdAt,author})
     <div className="post">
       <div className="image">
         <Link to={`/post/${_id}`}>
-          <img src={`${apiUrl}/${cover}`} alt="" />
+          {/* <img src={`${apiUrl}/${cover}`} alt="" /> */}
         </Link>
       </div>
       <div className="texts">
